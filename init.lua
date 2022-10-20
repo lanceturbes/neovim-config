b310c65fecce8df92768dde42bc41230bc7e0b5a
Lance Turbes <lturbes@mahalobanking.com>
2022-10-19 19:54:54 -0700

    feat: update plugins, docs; prevent crashes

--- init.lua
+++ init.lua
@@ -1,8 +1,12 @@
--- options
-local set = vim.opt
+-- helpers for setting options/keybindings
+local set = vim.opt -- set vim options
+local let = vim.g -- create vim variables
+local bind = vim.keymap.set -- create vim keybindings
 
+-- options
 set.autoindent = true -- inherit indent of previous lines
 set.backup = false -- don't save backup files
+set.completeopt = { "menu", "menuone", "noselect" } -- use autocomplete within these areas
 set.cursorline = true -- highlight line where cursor is
 set.expandtab = true -- use spaces instead of tabs
 set.hidden = true -- hide files in bg instead of closing them
@@ -23,7 +27,11 @@ set.tabstop = 4 -- set tab width to 4 spaces
 set.wildmenu = true -- allow tab autocomplete
 set.wrap = true -- wrap lines which hit screen edge
 
--- packer bootstrap
+-- variables
+let.mapleader = "<space>" -- set <leader> key to space bar -- default is backslash
+
+-- check if packer (plugin manager) is installed
+-- if not, download and install it automatically
 local ensure_packer = function()
 	local fn = vim.fn
 	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
@@ -35,11 +43,10 @@ local ensure_packer = function()
 	return false
 end
 
--- setup autocompletion
-vim.opt.completeopt = { "menu", "menuone", "noselect" }
-
-local cmp = require("cmp")
-if cmp then
+-- setup code completion
+local cmp_ok, _ = pcall(require, "cmp")
+if cmp_ok then
+	local cmp = require("cmp")
 	cmp.setup({
 		snippet = {
 			expand = function(args)
@@ -75,63 +82,75 @@ if cmp then
 	})
 end
 
--- Set up lspconfig.
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-local format_on_save_filter = function(client)
-	return client.name == "null-ls"
-end
+-- setup language servers (code highlighting/documentation helpers)
+local cmp_nvim_lsp_ok, _ = pcall(require, "cmp_nvim_lsp")
+if cmp_nvim_lsp_ok then
+	-- import default capabilities of neovim completion plugin
+	local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-local on_attach = function(client, bufnr)
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
-	vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
-	vim.keymap.set("n", "<space>df", vim.diagnostic.goto_next, { buffer = 0 })
+	-- set group to run formatting commands under
+	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-	-- format on save
-	if client.supports_method("textDocument/formatting") then
-		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = augroup,
-			buffer = bufnr,
-			callback = function()
-				vim.lsp.buf.format({ bufnr = bufnr, filter = format_on_save_filter })
-			end,
-		})
+	-- only format documents using null-ls (for eslint, prettier, etc.)
+	local format_on_save_filter = function(client)
+		return client.name == "null-ls"
 	end
-end
 
-require("lspconfig").tsserver.setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-})
+	-- function to run after a language server activates
+	local on_attach = function(client, bufnr)
+		-- setup keybinds for use with neovim's lsp
+		bind("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+		bind("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+		bind("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
+		bind("n", "<space>df", vim.diagnostic.goto_next, { buffer = 0 })
 
-require("lspconfig").sumneko_lua.setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	settings = {
-		Lua = {
-			runtime = {
-				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { "vim" },
-			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			-- Do not send telemetry data containing a randomized but unique identifier
-			telemetry = {
-				enable = false,
+		-- format on save
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr, filter = format_on_save_filter })
+				end,
+			})
+		end
+	end
+
+	-- configure typescript lsp
+	require("lspconfig").tsserver.setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+	})
+
+	-- configure lua lsp
+	require("lspconfig").sumneko_lua.setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+		settings = {
+			Lua = {
+				runtime = {
+					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+					version = "LuaJIT",
+				},
+				diagnostics = {
+					-- Get the language server to recognize the `vim` global
+					globals = { "vim" },
+				},
+				workspace = {
+					-- Make the server aware of Neovim runtime files
+					library = vim.api.nvim_get_runtime_file("", true),
+				},
+				-- Do not send telemetry data containing a randomized but unique identifier
+				telemetry = {
+					enable = false,
+				},
 			},
 		},
-	},
-})
+	})
+end
 
--- setup null-ls
+-- setup null-ls (to manage linting, formatting, diagnostics)
 local null_ls_ok, _ = pcall(require, "null-ls")
 if null_ls_ok then
 	local null_ls = require("null-ls")
@@ -146,11 +165,11 @@ if null_ls_ok then
 	})
 end
 
--- setup treesitter
+-- setup treesitter (fast/highly-detailed code highlighting)
 local treesitter_ok, _ = pcall(require, "nvim-treesitter")
 if treesitter_ok then
 	require("nvim-treesitter.configs").setup({
-		-- A list of parser names, or "all"
+		-- list of language parsers to install automatically
 		ensure_installed = {
 			"typescript",
 			"tsx",
@@ -204,7 +223,7 @@ if treesitter_ok then
 	})
 end
 
--- set colorscheme
+-- set color scheme/theme for the entire editor
 local colorscheme_ok, _ = pcall(vim.cmd, [[colorscheme tokyonight]])
 if colorscheme_ok then
 	vim.cmd([[colorscheme tokyonight]])
@@ -220,7 +239,7 @@ if telescope_ok then
 	vim.keymap.set("n", "fh", builtin.help_tags, {})
 end
 
--- setup lualine
+-- setup lualine (status line at the bottom of the editor)
 local lualine_ok, _ = pcall(require, "lualine")
 if lualine_ok then
 	require("lualine").setup({
@@ -230,38 +249,63 @@ if lualine_ok then
 	})
 end
 
--- setup plugins
+-- setup git signs (for line changes; displayed in left-side gutter)
+local gitsigns_ok, _ = pcall(require, "gitsigns")
+if gitsigns_ok then
+	require("gitsigns").setup()
+end
+
+-- setup packer (plugin manager)
 return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim") -- plugin manager
-	use("neovim/nvim-lspconfig") -- Configurations for Nvim LSP
-	use("hrsh7th/cmp-nvim-lsp") -- nvim cmp from content within lsp
-	use("hrsh7th/cmp-buffer") -- nvim cmp from content within buffer
-	use("hrsh7th/nvim-cmp") -- autocompletion
-	use({ "L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*" })
-	use("jose-elias-alvarez/null-ls.nvim") -- formatting/linting
+	-- plugin manager core
+	use("wbthomason/packer.nvim")
+
+	-- useful lua functions (similar to lodash; required by some plugins)
 	use("nvim-lua/plenary.nvim")
-	-- 	use({
-	-- 		"windwp/nvim-autopairs",
-	-- 		config = function()
-	-- 			require("nvim-autopairs").setup({})
-	-- 		end,
-	-- 	})
+
+	-- quick configuration plugin for language servers
+	use("neovim/nvim-lspconfig")
+
+	-- better syntax highlighting
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		run = function()
 			require("nvim-treesitter.install").update({ with_sync = true })
 		end,
 	})
+
+	-- code completion plugins for different editor areas
+	use("hrsh7th/cmp-nvim-lsp") -- nvim cmp from content within lsp
+	use("hrsh7th/cmp-buffer") -- nvim cmp from content within buffer
+	use("hrsh7th/nvim-cmp") -- autocompletion
+	use({ "L3MON4D3/LuaSnip", tag = "v1.1.*" }) -- snippet engine
+	use("jose-elias-alvarez/null-ls.nvim") -- formatting/linting
+
+	-- automatic pair closing (for quotes, parens, etc.)
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	})
+
+	-- fuzzy finder (similar to CTRL+P in vscode)
 	use({
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.0",
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
+
+	-- color schemes/statusline
 	use("folke/tokyonight.nvim")
 	use({
 		"nvim-lualine/lualine.nvim",
 		requires = { "kyazdani42/nvim-web-devicons", opt = true },
 	})
+
+	-- git integration plugins
+	use("lewis6991/gitsigns.nvim") -- add change indicators in the gutter
+	use("tpope/vim-fugitive") -- allow usage of git commands from command mode
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
